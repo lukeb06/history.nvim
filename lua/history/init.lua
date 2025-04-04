f2e6d63f428a15a105fd2178fdd5dd494beccf2d
Luke Barrier <lukebbarrier06@outlook.com>
2025-04-04 15:26:50 -0400

    Update

--- lua/history/init.lua
+++ lua/history/init.lua
@@ -1,9 +1,10 @@
 local M = {}
 
-M.buffers = {}
+M.history = {}
 
 M.icons = { enable = false, custom = {} }
 
+-- Returns a file handle to the workspace history file
 M.get_history_file = function(mode)
 	mode = mode or "w"
 	local data_dir = vim.fn.stdpath("data")
@@ -29,65 +30,70 @@ M.get_history_file = function(mode)
 	return io.open(path, mode)
 end
 
+-- Self-explanatory
 M.get_filepath_from_bufnr = function(bufnr)
-	local filename = vim.api.nvim_buf_get_name(bufnr)
-
-	return filename
+	return vim.api.nvim_buf_get_name(bufnr)
 end
 
-M.save_buffers = function()
-	local bufs = {}
-	for _, filename in ipairs(M.buffers) do
-		if vim.fn.filereadable(filename) == 1 then
-			table.insert(bufs, filename)
+-- Save the history to the workspace file
+M.save_history = function()
+	local files = {}
+	for _, filepath in ipairs(M.history) do
+		if vim.fn.filereadable(filepath) == 1 then
+			table.insert(files, filepath)
 		end
 	end
 
 	local file = M.get_history_file()
 
 	if file then
-		file:write(vim.fn.json_encode(bufs))
+		file:write(vim.fn.json_encode(files))
 		file:close()
 	end
 end
 
-M.load_buffers = function()
-	vim.schedule(function() -- Ensure this runs after startup
-		local file = M.get_history_file("r")
-		if not file then
-			return
+-- Load the history from the workspace file
+M.load_history = function()
+	vim.schedule(
+		function() -- Ensure this runs after startup. I'm actually not sure if this is necessary anymore but I'm too lazy to test it rn.
+			local file = M.get_history_file("r")
+			if not file then
+				return
+			end
+
+			local content = file:read("*a")
+			file:close()
+
+			local files = vim.fn.json_decode(content)
+			if not files or #files == 0 then
+				return
+			end
+
+			M.history = {}
+
+			for _, filepath in ipairs(files) do
+				M.add_file_to_history(filepath)
+			end
 		end
-
-		local content = file:read("*a")
-		file:close()
-
-		local bufs = vim.fn.json_decode(content)
-		if not bufs or #bufs == 0 then
-			return
-		end
-
-		M.buffers = {}
-
-		for _, filename in ipairs(bufs) do
-			M.add_file_to_history(filename)
-		end
-	end)
+	)
 end
 
--- Open the file and return the buffer number
-M.open_file = function(filename)
-	if vim.fn.filereadable(filename) == 1 then
-		vim.cmd("silent! edit " .. vim.fn.fnameescape(filename))
+-- Open the file and return the buffer number. Returns nil if the file doesn't exist.
+M.open_file = function(filepath)
+	if vim.fn.filereadable(filepath) == 1 then
+		vim.cmd("silent! edit " .. vim.fn.fnameescape(filepath))
 		return vim.fn.bufnr("%")
 	end
 
 	return nil
 end
 
-M.add_file_to_history = function(filename)
-	table.insert(M.buffers, filename)
+-- Self-explanatory
+M.add_file_to_history = function(filepath)
+	table.insert(M.history, filepath)
 end
 
+-- Self-explanatory
 M.get_filetype_from_filepath = function(filepath)
 	local filetype = vim.fn.fnamemodify(filepath, ":t:e")
 	return filetype
@@ -125,16 +131,16 @@ M.setup = function(opts)
 		callback = function()
 			local bufnr = vim.fn.bufnr("%")
 			local buftype = vim.api.nvim_buf_get_option(bufnr, "buftype")
-			local filename = M.get_filepath_from_bufnr(bufnr)
+			local filepath = M.get_filepath_from_bufnr(bufnr)
 
 			if buftype ~= "nofile" then
-				for i, buf in ipairs(M.buffers) do
-					if buf == filename then
-						table.remove(M.buffers, i)
+				for i, file in ipairs(M.history) do
+					if file == filepath then
+						table.remove(M.history, i)
 					end
 				end
 
-				M.add_file_to_history(filename)
+				M.add_file_to_history(filepath)
 			end
 		end,
 	})
@@ -143,14 +149,14 @@ M.setup = function(opts)
 		vim.api.nvim_create_autocmd("VimLeavePre", {
 			callback = function()
 				if vim.fn.argc(-1) == 0 then
-					M.save_buffers()
+					M.save_history()
 				end
 			end,
 		})
 		vim.api.nvim_create_autocmd("VimEnter", {
 			callback = function()
 				if vim.fn.argc(-1) == 0 then
-					M.load_buffers()
+					M.load_history()
 				end
 			end,
 		})
@@ -187,10 +193,10 @@ M.setup = function(opts)
 			M.icons.enable = false
 		end
 
-		for _, name in ipairs(M.buffers) do
-			if vim.fn.filereadable(name) == 1 then
+		for _, filepath in ipairs(M.history) do
+			if vim.fn.filereadable(filepath) == 1 then
 				local cwd = vim.fn.getcwd()
-				local ft = M.get_filetype_from_filepath(name)
+				local ft = M.get_filetype_from_filepath(filepath)
 
 				-- Get icon with custom override
 				local icon, hl = " ", "Normal"
@@ -199,18 +205,18 @@ M.setup = function(opts)
 					if M.icons.custom[ft] then
 						icon = M.icons.custom[ft]
 					else
-						icon, hl = web_devicons.get_icon(name, ft)
+						icon, hl = web_devicons.get_icon(filepath, ft)
 					end
 					icon = icon or " " -- Fallback to space if no icon found
 				end
 
 				local match = escape_pattern(cwd .. "/")
-				name = name:gsub(match, "")
+				filepath = filepath:gsub(match, "")
 
 				-- Split path components
-				local dir, filename = name:match("^(.*/)([^/]+)$")
+				local dir, filename = filepath:match("^(.*/)([^/]+)$")
 				dir = dir or ""
-				filename = filename or name
+				filename = filename or filepath
 
 				-- Create styled line
 				local line = NuiLine()
@@ -221,7 +227,7 @@ M.setup = function(opts)
 				line:append(NuiText(dir, "Comment"))
 				line:append(NuiText(filename, "Normal"))
 
-				local item = Menu.item(line, { filename = name })
+				local item = Menu.item(line, { filpath = filepath })
 				table.insert(lines, item)
 			end
 		end
@@ -255,7 +261,7 @@ M.setup = function(opts)
 			end,
 			on_submit = function(item)
 				-- vim.api.nvim_set_current_buf(item.bufnr)
-				M.open_file(item.filename)
+				M.open_file(item.filepath)
 			end,
 		})
 
